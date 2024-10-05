cf5e1bea699d48f5b1752cf0e8276fedb85911eb
Steel <Steelmen87@bk.ru>
2024-10-05 12:53:23 +0300

    dev 1/2

--- src/App.tsx
+++ src/App.tsx
@@ -8,14 +8,7 @@ function App() {
       <header className="App-header">
 
 
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
     </div>
   );
